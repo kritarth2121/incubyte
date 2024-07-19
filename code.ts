ba958a7bb5d575cfce95e0ef368854add99a7358
Kritarth Sharma <kritarth.sharma@devslane.com>
2024-07-19 19:50:46 +0530

    step 4 Support different delimiters:
    To change the delimiter, the beginning of the string will contain a separate line that looks like this: "//[delimiter]\n[numbers…]". For example, "//;\n1;2" where the delimiter is ";" should return 3.

--- code.ts
+++ code.ts
@@ -3,8 +3,15 @@ function add(numbers: string): number {
       return 0;
   }
 
-  // Replace new lines with commas and split the string
-  const numberArray = numbers.replace(/\n/g, ',').split(',').map(num => parseInt(num, 10));
+  let delimiter = ',';
+  if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbers = numbers.substring(delimiterEndIndex + 1);
+  }
+
+  // Replace new lines with the delimiter and split the string
+  const numberArray = numbers.replace(/\n/g, delimiter).split(delimiter).map(num => parseInt(num, 10));
 
   // Filter out any NaN values that may result from parsing non-numeric strings
   const filteredNumbers = numberArray.filter(num => !isNaN(num));
@@ -20,3 +27,6 @@ console.log(add("1\n2")); // Output: 3
 console.log(add("1\n2,3")); // Output: 6
 console.log(add("1,2,3")); // Output: 6
 console.log(add("1\n2\n3\n4,5")); // Output: 15
+console.log(add("//;\n1;2")); // Output: 3
+console.log(add("//|\n1|2|3")); // Output: 6
+console.log(add("//sep\n2sep3")); // Output: 5
